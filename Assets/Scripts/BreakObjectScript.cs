07758b570a0a5f28479afc695504ce73a93095a8
BlackKronos2 <34336340+BlackKronos2@users.noreply.github.com>
2022-11-27 02:34:19 +0700

    Release
    Release Breaking Glass Pack for Unity5

---
+++ Assets/Scripts/BreakObjectScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakObjectScript : MonoBehaviour
+{
+	[SerializeField]
+	private bool solid;
+	[SerializeField]
+	private GameObject brokenObject;
+	[SerializeField]
+	private float maximum_magnitude;
+	[SerializeField]
+	private float radius;
+	[SerializeField]
+	private float power;
+	[SerializeField]
+	private float upwards;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (Mathf.Abs(collision.relativeVelocity.magnitude) > maximum_magnitude)
+		{
+			Vector3 collision_position = collision.transform.position;
+			GameObject broken_object = Instantiate(brokenObject, transform.position, transform.rotation);
+			broken_object.transform.localScale = transform.localScale;
+
+
+			Collider[] colliders = Physics.OverlapSphere(collision_position, radius);
+
+
+			foreach (var piece in colliders) {
+				var rigibody = piece.GetComponent<Rigidbody>();
+				if (rigibody != null && solid)
+				{
+					rigibody.AddExplosionForce(power * collision.relativeVelocity.magnitude, collision_position, radius);
+				}
+
+			}
+
+			Destroy(gameObject);
+		}
+	}
+}
